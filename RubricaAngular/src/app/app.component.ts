35ae9bd2f2f246b49969cc91c5c2b88f846e6f68
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-20 14:30:02 +0100

    fdof

--- RubricaAngular/src/app/app.component.ts
+++ RubricaAngular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactsListComponent } from "./contacts-list/contacts-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- RubricaAngular/src/app/contacts-list/contacts-list.component.ts
+++ RubricaAngular/src/app/contacts-list/contacts-list.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Contact } from '../models/Contact';
 
 @Component({
   selector: 'app-contacts-list',
-  imports: [],
+  standalone: true,  
+  imports: [CommonModule, FormsModule],  
   templateUrl: './contacts-list.component.html',
-  styleUrl: './contacts-list.component.css'
+  styleUrls: ['./contacts-list.component.css']
 })
-export class ContactsListComponent {
 
+
+export class ContactsListComponent {
+  contacts: Contact[] = [];
+  errorMessage: string = "";
+
+  newContact: Contact = { name: '', phone: '', extra: '' };
+
+  addContact() {
+    if (this.newContact.name && this.newContact.phone) {
+      this.errorMessage = "";
+      this.contacts.push({ ...this.newContact });
+      this.newContact = { name: '', phone: '', extra: '' }; // Reset form
+    }
+    else{
+      this.errorMessage = "inserisci numero e telefono";
+    }
+  }
+
+  removeContact(index: number) {
+    this.contacts.splice(index, 1);
+  }
 }
